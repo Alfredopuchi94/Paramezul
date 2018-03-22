7daf52ac54e77402b0aa56f63cfcf2d76be6997b
Marco Montilla <marco.jr1695@gmail.com>
2018-03-22 17:13:46 -0400

    Adecuacion del codigo para las notificaciones

--- servicio.php
+++ servicio.php
@@ -1,5 +1,5 @@
 
-<?php 
+<?php
 session_start();
 		$status = false;
 
@@ -17,7 +17,7 @@ session_start();
 			}
 		}
 		?>
-<?php 
+<?php
 if ($_COOKIE['email']) {
 ?>
 <!DOCTYPE html>
@@ -35,143 +35,101 @@ if ($_COOKIE['email']) {
 	<?php include "src/app_api/config/connection.php" ?>
 	<header>
 		<nav class="navbar navbar-dark bg-dark navbar-expand-lg justify-content-between">
-  <h3 class="navbar-brand justify-content-center mx-auto" href="/">Paramezul</h3>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+		  <h3 class="navbar-brand justify-content-center mx-auto" href="/">Paramezul</h3>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
 
-  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-    <!-- <ul class="navbar-nav mr-auto"> -->
-    	<ul class="navbar-nav ">
-		      <li class=" nav-item">
-		        <a class="nav-link" href="#"><span class="fa fa-home"> Home</span></a>
-		      </li>
-		 </ul>
-		<ul class="navbar-nav ">
-			<form class="form-inline">
-				<?php if ($status) { ?>
-					<div class="input-group">
-						<input type="text" value="Hi <?php echo $_SESSION['fname'].' '.$_SESSION['lname']; ?>" class="form-control text-center" readonly>
-			      <span class="input-group-btn">
-			        <a href="./logout" class="btn btn-outline-danger">Cerrar Sesión</a>
-			      </span>
-			    </div>
-				<?php } ?>
-			</form>
-    	</ul>
-  </div>
-</nav>
+		  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+				<ul class="navbar-nav ">
+				    <li class=" nav-item">
+				      <a class="nav-link" href="#"><span class="fa fa-home"> Home</span></a>
+				    </li>
+				</ul>
+				<ul class="navbar-nav ">
+					<form class="form-inline">
+						<?php if ($status) { ?>
+							<div class="input-group">
+								<input type="text" value="Hi <?php echo $_SESSION['fname'].' '.$_SESSION['lname']; ?>" class="form-control text-center" readonly>
+					      <span class="input-group-btn">
+					        <a href="./logout" class="btn btn-outline-danger">Cerrar Sesión</a>
+					      </span>
+					    </div>
+						<?php } ?>
+					</form>
+		    </ul>
+		  </div>
+		</nav>
 	</header>
 
 	<div class="container">
 		<div class="row my-5">
 			<div class="col-md-8 offset-md-2 my-5">
 				<div class="row">
-				<div class="mx-auto">	
-				<!-- Button trigger modal -->
-					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter1">
-					  Servicio Basico
-					</button>
-					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter2">
-					  Servicio Avanzado 
-					</button>
-				</div>
-
+					<div class="mx-auto">
+						<!-- Button trigger modal -->
+						<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter1">
+						  Servicio Basico
+						</button>
+					</div>
 					<!-- Modal 1 -->
-					<div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-					  <div class="modal-dialog modal-dialog-centered" role="document">
-					    <div class="modal-content">
-					      <div class="modal-header">
-					        <h5 class="modal-title text-center" id="exampleModalLongTitle" style="color: rgb(25, 144, 206);">Servicio Basico</h5>
-					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					          <span aria-hidden="true">&times;</span>
-					        </button>
-					      </div>
-					      	<div class="modal-body" style="background-color: rgba(25, 144, 206,.3);">
-					      	 	<div class="container">
-									<div class="row">
-										<div class="col-md-8 offset-md-2">
-											<form class="form-horizontal" id="formservicio" action="src\app_api\php\datos.php" method="post" name="formservicio">
-												<div id="form_message"></div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="nombre_cliente" id="nombre_cliente" value="<?php echo $_SESSION["fname"].' '.$_SESSION['lname'] ?>" disabled>
-												</div>
-											    <div class="form-group">
-													<input type="text" class="form-control" name="nombre_pasiente" id="nombre_pasiente" placeholder="Nombre del paciente ">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="diagnostico" id="diagnostico" placeholder="Diagnostico">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="ubi_salida" id="ubi_salida" placeholder="ubicacion salida">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="ubi_destino" id="ubi_destino" placeholder="ubicacion destino ">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="telefono" id="telefono" placeholder="Telefono">
-												</div>	
-												<input type="hidden" name="type" value="basico">
-											</div>
-					     	 			</div>
-					     	 		</div>
-					     		</div>
-					      <div class="modal-footer">
-					      	<input type="hidden" name="id" value="formservicio">
-					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-					        <button type="submit" class="btn btn-primary">Solicitar</button>
-					      </div>
-					  </form>
-					    </div>
-					  </div>
-					</div>
-					<!-- Modal 2 -->
-					<div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-					  <div class="modal-dialog modal-dialog-centered" role="document">
-					    <div class="modal-content">
-					      <div class="modal-header">
-					        <h5 class="modal-title" id="exampleModalLongTitle" style="color: rgb(2, 184, 191);"> Servicio Avanzado</h5>
-					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					          <span aria-hidden="true">&times;</span>
-					        </button>
-					      </div>
-					      	<div class="modal-body">
-					      	 	<div class="container">
-									<div class="row">
-										<div class="col-md-8 offset-md-2">
-											<form class="form-horizontal" id="Usuarios" action="src/app_api/php/datos.php" method="post" name="Usuarios">
-												<div id="form_message"></div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre y Apellido">
-												</div>
-											    <div class="form-group">
-													<input type="text" class="form-control" name="email" id="email" placeholder="Nombre del paciente ">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="email" id="email" placeholder="Diagnostico">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="email" id="email" placeholder="ubicacion salida">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="email" id="email" placeholder="ubicacion destino ">
-												</div>
-												<div class="form-group">
-													<input type="text" class="form-control" name="email" id="email" placeholder="Telefono">
-												</div>
-												<input type="hidden" name="type" value="avanzado">	
-											</div>
-					     	 			</div>
-					     	 		</div>
-					     		</div>
-					      <div class="modal-footer">
-					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-					        <button type="submit" class="btn btn-primary">Solicitar</button>
-					      </div>
-					  </form>
-					    </div>
-					  </div>
-					</div>
+						<div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						  <div class="modal-dialog modal-dialog-centered" role="document">
+						    <div class="modal-content">
+									<form class="form-horizontal" id="formservicio">
+						      	<div class="modal-header">
+						        <h5 class="modal-title text-center" id="exampleModalLongTitle" style="color: rgb(25, 144, 206);">Servicio Basico</h5>
+						        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						          <span aria-hidden="true">&times;</span>
+						        </button>
+									</div>
+						      	<div class="modal-body" style="background-color: rgba(25, 144, 206,.3);">
+						      	 	<div class="container">
+												<div class="row">
+													<div class="col-md-8 offset-md-2">
+															<div id="form_message"></div>
+
+															<input
+																type="hidden"
+																name="id_cliente"
+																id="id_cliente"
+																value="<?php echo $_SESSION["__id"]; ?>"
+															>
+
+														  <div class="form-group">
+																	<input
+																		type="text"
+																		class="form-control"
+																		name="telf_contacto"
+																		id="telf_contacto"
+																		placeholder="Tel a donde sera contactado">
+															</div>
+
+															<div class="form-group">
+														    <label for="ubicacion">Ubicacion</label>
+														    <textarea class="form-control" name="ubicacion" id="ubicacion" placeholder="Ubicacion" rows="3"></textarea>
+														  </div>
+
+															<div class="form-group">
+														    <label for="tipo">Tipo de servicio</label>
+														    <select class="form-control" id="tipo" name="tipo">
+														      <option value="basico">Basico</option>
+														      <option value="avanzado">Avanzado</option>
+														    </select>
+														  </div>
+													</div>
+							     	 		</div>
+						     	 		</div>
+						     		</div>
+							      <div class="modal-footer">
+							      	<input type="hidden" name="id" value="formservicio">
+							        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+							        <button type="submit" class="btn btn-primary">Solicitar</button>
+							      </div>
+							  	</form>
+						    </div>
+						  </div>
+						</div>
 				</div>
 			</div>
 		</div>
---
+++ src/app_api/modules/cliente/crud/solicitud.php
@@ -0,0 +1,35 @@
+<?php
+	require_once "../../../config/connection.php";
+	require_once "../../methods/cliente.php";
+	require_once "../../methods/common.php";
+
+	$res = [];
+
+	$obj = new Methods();
+	$resp = $obj->tryToConnect();
+	$res['connection'] = $resp;
+
+	if(!empty($_POST)){
+
+		$id = $_POST["id_cliente"];
+		$telf = $_POST["telf_contacto"];
+		$ubicacion = $_POST["ubicacion"];
+		$tipo = $_POST["tipo"];
+
+		if(isset($id) && isset($telf) && isset($ubicacion) && isset($tipo)){
+
+			if($id!="" && $telf!="" && $ubicacion!="" && $tipo!=""){
+
+				$obj = new Methods();
+				$resp = $obj->notificar('notificacion', $id, $telf, $ubicacion, $tipo);
+
+				if ($resp){
+					$res['sent'] = true;
+				} else{
+					$res['sent'] = false;
+				}
+			}
+		}
+	}
+
+	echo json_encode($res);
